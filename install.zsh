7955fe17c913bdee3c82cc3d19a27ee30c4afc3f
Alberts Zemzale <alberts.zemzale@gmail.com>
2021-12-14 18:10:23 +0200

    Install gh

--- install.zsh
+++ install.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-brew install python node golang thefuck tmux fzf exa bat rg
+brew install python node golang thefuck tmux fzf exa bat rg gh
 
 # TODO: check if this exists
 /usr/local/opt/fzf/install
