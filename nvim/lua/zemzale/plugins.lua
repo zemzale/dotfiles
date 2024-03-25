fd81044da08fd2f3a8a88c87b3086b32905e7008
Alberts Zemzale <alberts.zemzale@gmail.com>
2024-03-25 08:39:10 +0200

    Clean up nvim config

--- nvim/lua/zemzale/plugins.lua
+++ nvim/lua/zemzale/plugins.lua
@@ -1,82 +1,80 @@
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-			},
-			{ "kyazdani42/nvim-web-devicons" },
-		},
-	})
-	use({
-		"jay-babu/mason-null-ls.nvim",
-		requires = {
-			"jose-elias-alvarez/null-ls.nvim",
-		},
-	})
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{
-				-- Optional
-				"williamboman/mason.nvim",
-				build = function()
-					pcall(vim.cmd, "MasonUpdate")
-				end,
-			},
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        requires = {
+            { "nvim-lua/plenary.nvim" },
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                run =
+                "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+            },
+            { "kyazdani42/nvim-web-devicons" },
+        },
+    })
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        requires = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" }, -- Required
+            {
+                -- Optional
+                "williamboman/mason.nvim",
+                build = function()
+                    pcall(vim.cmd, "MasonUpdate")
+                end,
+            },
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "L3MON4D3/LuaSnip" }, -- Required
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-		},
-	})
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		requires = {
-			{ "nvim-treesitter/playground" },
-			{ "nvim-treesitter/nvim-treesitter-context" },
-		},
-	})
-	use({
-		"ThePrimeagen/harpoon",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-		},
-	})
-	use({
-		"lewis6991/gitsigns.nvim",
-		tag = "release",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	})
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
-	use({ "folke/tokyonight.nvim", branch = "main" })
-	use({ "tpope/vim-fugitive" })
-	use({
-		"fatih/vim-go",
-		config = function() end,
-	})
-	use({
-		"github/copilot.vim",
-	})
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },     -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "L3MON4D3/LuaSnip" },     -- Required
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
+
+            -- Formatting
+            "nvimtools/none-ls.nvim"
+        },
+    })
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        requires = {
+            { "nvim-treesitter/playground" },
+            { "nvim-treesitter/nvim-treesitter-context" },
+        },
+    })
+    use({
+        "ThePrimeagen/harpoon",
+        requires = {
+            { "nvim-lua/plenary.nvim" },
+        },
+    })
+    use({
+        "lewis6991/gitsigns.nvim",
+        tag = "release",
+        config = function()
+            require("gitsigns").setup()
+        end,
+    })
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    })
+    use({ "folke/tokyonight.nvim", branch = "main" })
+    use({ "tpope/vim-fugitive" })
+    use({
+        "fatih/vim-go",
+        config = function() end,
+    })
+    use({
+        "github/copilot.vim",
+    })
 end)
